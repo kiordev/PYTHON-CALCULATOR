3e6a6ad6875b4ab84c8d7e7167ed53c584a731d0
kiordev <kiordev@gmail.com>
2022-04-12 21:49:00 +0300

    default

--- main.py
+++ main.py
@@ -2,4 +2,25 @@
 # Creator: Alexandr Kior kiordev@gmail.com
 # Date: April 12 2022
 
+import sys
+import PySide6.QtCore
+import PySide6.QtGui
+from PySide6.QtWidgets import QApplication, QMainWindow
 
+
+from design import Ui_MainWindow
+
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super(Calculator, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = Calculator()
+    window.show()
+    sys.exit(app.exec())
